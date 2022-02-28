ec65b542c98d276eca3eb6ac692823f8c075cdf6
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2022-03-01 01:03:13 +0530

    Documentation Fixes

--- app/app.js
+++ app/app.js
@@ -4,24 +4,27 @@
 
 angular.module('miracurolApp', ['ui.router'])
     .config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+        // Enable HTML-5 Mode
         $locationProvider.html5Mode({
             enabled: true, requireBase: false
         });
-        // $urlRouterProvider.otherwise('/');
+
+        // Default to 'https://<base url>/'
+        $urlRouterProvider.otherwise('/');
 
         // creating routes or states
         $stateProvider
             .state({
                 name: 'home', url: '/', views: {
                     "content": {
-                        template: '<h1>This is Home Page</h1>',
+                        template: '<h1 class="mt-4">This is Home Page</h1>',
                     }
                 },
             })
             .state({
                 name: 'about', views: {
                     "content": {
-                        template: '<h1>This is About Page</h1>',
+                        template: '<h1 class="mt-4">This is About Page</h1>',
                     }
                 },
             });
