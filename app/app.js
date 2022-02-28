fd0f389e5bb90b46d1afb3a950ffa04cfc1cf43b
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2022-02-28 21:48:30 +0530

    SB-Admin Theme Added

--- app/app.js
+++ app/app.js
@@ -1,29 +1,25 @@
 /*global angular*/
 
-"use strict";
+'use strict';
 
 angular.module('miracurolApp', ['ui.router'])
     .config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
         $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: false
+            enabled: true, requireBase: false
         });
         // $urlRouterProvider.otherwise('/');
 
         // creating routes or states
         $stateProvider
             .state({
-                name: 'home',
-                url: '/',
-                views: {
+                name: 'home', url: '/', views: {
                     "content": {
                         template: '<h1>This is Home Page</h1>',
                     }
                 },
             })
             .state({
-                name: 'about',
-                views: {
+                name: 'about', views: {
                     "content": {
                         template: '<h1>This is About Page</h1>',
                     }
